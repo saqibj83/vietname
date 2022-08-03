2b4536c493d6f32c46d398cddd2a88b3f5307b22
Muhammad Saqib <saqib2uk@hotmail.com>
2022-08-03 12:12:31 +0500

    fb-pixel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,32 @@
-import { Modal } from '@redq/reuse-modal';
-import '@redq/reuse-modal/es/index.css';
-import 'common/assets/css/flaticon.css';
-import '../containers/CryptoModern/CountDown/timer.css';
-import 'common/assets/css/icon-example-page.css';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { Modal } from "@redq/reuse-modal";
+import "@redq/reuse-modal/es/index.css";
+import "common/assets/css/flaticon.css";
+import "../containers/CryptoModern/CountDown/timer.css";
+import "common/assets/css/icon-example-page.css";
 // swiper bundle styles
-import 'swiper/css/bundle';
-import 'common/assets/css/react-slick.css';
-import 'common/assets/css/rc-collapse.css';
-import 'rc-collapse/assets/index.css';
+import "swiper/css/bundle";
+import "common/assets/css/react-slick.css";
+import "common/assets/css/rc-collapse.css";
+import "rc-collapse/assets/index.css";
 
 export default function CustomApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("550701485702284"); // facebookPixelId
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
+
   return (
     <Modal>
       <Component {...pageProps} />
