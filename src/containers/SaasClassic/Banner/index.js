c26b58447a4cb3606955e46cd71199033b421ab7
Muhammad Saqib <saqib2uk@hotmail.com>
2023-03-07 10:21:57 +0500

    youtube-6

--- src/containers/SaasClassic/Banner/index.js
+++ src/containers/SaasClassic/Banner/index.js
@@ -22,7 +22,7 @@ const CloseModalButton = () => (
 
 const ModalContent = () => (
   <VideoWrapper>
-    <iframe title="Video" src="https://www.youtube.com/embed/rYEH30AS4Yw" />
+    <iframe title="Video" src="https://www.youtube.com/embed/ens1f5xRg04" />
   </VideoWrapper>
 );
 
@@ -61,7 +61,7 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
           <div className="video-section">
             <Fade bottom>
               <ReactPlayer
-                url="https://www.youtube.com/embed/rYEH30AS4Yw"
+                url="https://www.youtube.com/embed/ens1f5xRg04"
                 // light="https://cdn.triton-series.com/wp-content/uploads/2019/12/05122106/bg-vietnam.png"
                 playing
                 width="711px"
